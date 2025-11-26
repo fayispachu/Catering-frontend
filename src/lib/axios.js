4bad0c786603fcdf10273a58c8763ba5a04ae59c
Fayis <fayizpachu217@gmail.com>
2025-11-26 16:05:40 +0530

    render

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://catering-backend-l91k.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
